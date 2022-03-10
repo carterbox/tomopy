7b4de3180fa90823d9447ea34904ebbc1fbb850f
Daniel Ching <carterbox@users.noreply.github.com>
2022-03-10 17:48:51 -0600

    Add missing python function

--- source/tomopy/util/extern/recon.py
+++ source/tomopy/util/extern/recon.py
@@ -569,6 +569,37 @@ def c_vector3(tomo1, tomo2, tomo3, center1, center2, center3, recon1, recon2,
                                     dtype.as_c_int(axis3))
 
 
+def FFT_order(x):
+    """Reorders x according to the 1D Cooley-Tukey FFT access pattern"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+    if N % 2 > 0:
+        raise ValueError("size of x must be a power of 2")
+    elif N <= 2:  # this cutoff should be optimized
+        return x
+    else:
+        X_even = FFT_order(x[::2])
+        X_odd = FFT_order(x[1::2])
+        return np.concatenate([X_even, X_odd])
+
+
+def multilevel_order(L):
+    """Returns an order of length L according to the multilevel scheme by
+    Guan and Gordon (1994)
+    """
+    if L % 2 > 0:
+        raise ValueError("L must be a power of 2")
+    N = 2
+    order = list()
+    order.append(np.array([0, 1]) / 2)
+    level = 4
+    while N < L:
+        order.append(FFT_order(np.arange(1, level, 2)) / level)
+        N += level / 2
+        level *= 2
+    return (np.concatenate(order) * L).astype('int32')
+
+
 def c_art_convolve(tomo, center, recon, theta, **kwargs):
     if len(tomo.shape) == 2:
         # no y-axis (only one slice)
